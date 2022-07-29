a0d44f39373e37003c45c67de6da5142cc28d356
Quý <quycao97@gmail.com>
2022-07-29 18:42:46 +0700

    tinh-tong

---
+++ tesst-for.js
@@ -0,0 +1,18 @@
+// for (var i=0; i<5; i++){
+//             document.write("hello");
+            
+//         }
+// var a = 0 ;
+// while (a < 5){
+//     a++;
+//     document.write("hello ");
+// }
+// alert("Ban da nhap vao so duong")
+// do {
+//     alert("Ban da nhap vao so " + a)
+// }
+
+// while (a<0);
+for (var a =-1; a < 0 || a > 10; ){
+    var a = +prompt("Moi nhap lai a")
+}
